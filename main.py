4f9e87c224706fc106123a08cdf4495e9d6302d5
shelzyxpython <144206667+shelzyxpython@users.noreply.github.com>
2023-10-10 19:50:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+#num_1,num_2 = int(input()), int(input())
+#for i in range(num_1,num_2 + 1):
+#    print(i)
+
+#num_1,num_2 = int(input()), int(input())
+#if num_1 < num_2:
+#    for i in range(num_1,num_2 + 1):
+#        print(i)
+#else:
+#    for i in range(num_1,num_2 - 1, -1):
+#        print(i)
+
+#n = int(input())
+#sum = 0
+#for i in range(n):
+#    num = int(input())
+#    sum += num
+#print(sum)
+
+#num = int(input())
+#sum = 1
+#for i in range(1,num + 1):
+#    sum *= i
+#print(sum)
+
+#n = int(input())
+#for i in range(1,n + 1):
+#    for q in range(1, i+1):
+#        print(q, end ='')
+#    print()
+
+#n = int(input())
+#x = 1
+#while x**2 < n:
+#    print(x**2)
+#    x += 1
+
+#x = float(input()) #с integer происходят проблемы, без конца значения вводятся, с float нет проблем, но вместо 0.1 надо 1.1
+#y = float(input())
+#a = x
+#d = 1
+#while a < y:
+#    a *= 1.1
+#    d += 1
+#print(d)
+
+#x = int(input())
+#y = int(input())
+#a = x
+#day = 1
+#while a <= y:
+#    a *= 0.1
+#    day += 1
+#print(day)
+
+#num = 0
+#while True:
+#    num_1 = int(input())
+#    if num_1 == 0:
+#        break
+#    num += 1
+#print(num)
+
+#a = int(input())
+#num = 0
+#num_1 = 1
+#for _ in range(2, a + 1):
+#    num_2 = num + num_1
+#    num = num_1
+#    num_1 = num_2
+#result = num_1
+#print(result)
+
